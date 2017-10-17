e6b32698b14efb4c5ce298c0c9a963bc5862749a
Hinaser <rindo.hinase@gmail.com>
2017-10-17 19:15:05 +0900

    WIP: styling headline

--- src/sssg/js/standard/components/sidebar.js
+++ src/sssg/js/standard/components/sidebar.js
@@ -1,5 +1,14 @@
 export default class Sidebar {
   constructor(){
     this.selector = "body > main > nav";
+    
+    this.wrapHeadline();
+  }
+  
+  wrapHeadline(){
+    let headlineTitle = $(this.selector).find(".headline .headline-title");
+    headlineTitle.dotdotdot({
+      truncate: "letter"
+    });
   }
 }
